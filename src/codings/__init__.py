367fb2131c1b17af96d17b1f8ee27a2ae2762a4d
GeKeShi <gekeshi@gmail.com>
2020-12-29 23:21:24 +0800

    add count min and count sketch class

--- src/codings/__init__.py
+++ src/codings/__init__.py
@@ -2,6 +2,8 @@ from .coding import Coding
 from .svd import SVD
 from .qsgd import QSGD
 from .LSS import LSS
+from cmvec import CMVec
+from csvec import CSVec
 import utils
 
 __all__ = ['coding', 'svd', 'qsgd', 'utils', 'LSS']
